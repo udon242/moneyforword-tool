8b653a31de334cc1c5c1fd54711ab9ecda9e31cf
udon11 <ylu242@gmail.com>
2019-02-02 14:45:00 +0900

    [add] 今月の収支を出力する

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # moneyforword-tool
+
+## Run
+
+```
+LOGIN_EMAIL=<email> LOGIN_PASSWORD=<password> npx ts-node index.ts
+```
---
+++ index.ts
@@ -0,0 +1,59 @@
+import * as puppeteer from 'puppeteer';
+
+const MF_URL = 'https://moneyforward.com';
+const LOGIN_EMAIL = process.env.LOGIN_EMAIL;
+const LOGIN_PASSWORD = process.env.LOGIN_PASSWORD;
+
+(async () => {
+    try {
+        // アクセス
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(`${MF_URL}/users/sign_in`, {
+            waitUntil: 'networkidle2', // 画像, JSの読み込み後まで待つ
+        });
+
+        // Email, Passwordを入力
+        await page.type('input[id="sign_in_session_service_email"]', LOGIN_EMAIL);
+        await page.type('input[id="sign_in_session_service_password"]', LOGIN_PASSWORD);
+
+        // ログインボタンをクリック
+        await page.click('input[id="login-btn-sumit"]');
+
+        // ページ遷移を待つ
+        await page.waitForNavigation();
+        await page.screenshot({
+            path: 'screenshot/login.png',
+        });
+
+        // 家計ページへ遷移
+        await page.goto(`${MF_URL}/cf`, {
+            waitUntil: 'networkidle2',
+        });
+
+        await page.screenshot({
+            path: 'screenshot/cf.png',
+        });
+
+        // evaluateでページ要素を抜き出す
+        const monthlyTotalList = await page.evaluate(() => {
+            const dataList: Array<String> = [];
+            const nodeList = document.querySelectorAll('.js-monthly_total td');
+            nodeList.forEach(_node => {
+                const tdValue = _node.innerHTML;
+                dataList.push(tdValue);
+            });
+            return dataList;
+        });
+
+        console.log(`今月の収入: ${monthlyTotalList[0]}`);
+        console.log(`今月の支出: ${monthlyTotalList[2]}`);
+        console.log(`今月の収支: ${monthlyTotalList[4]}`);
+
+        // ブラウザを閉じる
+        await browser.close();
+    } catch (error) {
+        console.error('[Error]');
+        console.error(error);
+    }
+})();
